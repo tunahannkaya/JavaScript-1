d4a57cdd39270d95c2531d113b45e687632bbbdc
tunahannkaya <152183813+tunahannkaya@users.noreply.github.com>
2024-10-10 01:39:40 +0300

    First try
    İlk denemeler

---
+++ Script1.js
@@ -0,0 +1,23 @@
+// JavaScript source code
+< !doctype html >
+
+<html lang="tr-TR">
+
+    <head>
+   
+
+        <meta http-equiv="content-type" content="text/html ; charset=utf-8">
+
+            <title>Tunahan Kaya</title>
+            <style type="text/css">
+
+                body {color : blue ;}
+
+            </style>
+
+    </head>
+
+    <body style="background-color :white;">
+    Merhaba
+    </body>
+</html>
