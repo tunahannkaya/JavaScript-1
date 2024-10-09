feb19aedc7088435505f6bb2164751cf4ca307cf
tunahannkaya <152183813+tunahannkaya@users.noreply.github.com>
2024-10-10 01:57:11 +0300

    try
    Merhaba

---
+++ Script2.js
@@ -0,0 +1,20 @@
+// JavaScript source code
+< !DOCTYPE html >
+
+    <html lang="tr-TR">
+        <head>
+
+            <link type="text=css" rell="styless" herf="43.css">
+
+            <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+            <title>Tunahan KAYA</title>
+
+         </head>
+         <body>
+            merhaba<br />          
+            <div id="denemes">Merhaba</div>            
+          </body>             
+
+
+    </html>
