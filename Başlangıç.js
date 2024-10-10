244af98e8cd50c96a2c9a8235e88fd077f48ac3e
tunahannkaya <152183813+tunahannkaya@users.noreply.github.com>
2024-10-10 13:02:34 +0300

    Studies
    Başlangıç seviyesi Java

---
+++ Başlangıç.js
@@ -0,0 +1,25 @@
+// JavaScript source code
+< !DOCTYPE html >
+
+    <html lang="tr-TR">
+        <head>
+            <meta equiv="content-type" content="text/html ; charset=utf-8">
+             <title>Tunahan KAYA</title>
+
+        </head>
+
+        <body>
+            <script type="tpye/javascript" lang="javascript">
+
+                var isim = "Tunahan" ; 
+                var soyisim = "KAYA" ;
+                var eðitim = "JavaScript" ;
+                var seviye = "Baþlangýç" ;
+                alert (seviye);
+
+            </script>
+
+        </body>
+
+
+    </html>
