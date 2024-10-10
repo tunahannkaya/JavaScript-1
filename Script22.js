87ca0b330ba31c6eb56cdf21856b305aa568121e
tunahannkaya <152183813+tunahannkaya@users.noreply.github.com>
2024-10-10 12:41:52 +0300

    Studies
    İlk oluşumlar

---
+++ Script22.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+< !DOCTYPE html >
+
+    <html lang="tr-TR">
+        <head>
+            <meta equiv="content-type" lang="javascript">
+            <title>Tunahan KAYA</title>
+            <script type="text/javascript" lang="javascript">
+                alert("Hoþgeldiniz");
+            </script>
+
+        </head>
+        <body></body>
+    </html>
