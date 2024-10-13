83b486d8dc1444fde1baa3d172202c1baf579758
tunahannkaya <152183813+tunahannkaya@users.noreply.github.com>
2024-10-13 12:13:20 +0300

    Studies
    Çalışmalar

---
+++ çalışma.js
@@ -0,0 +1,30 @@
+// JavaScript source code
+< !DOCTYPE html >
+<html lang="tr-TR">
+
+        <head>
+
+            <meta http-equiv="content-type" content="text/html ; charset=utf-8">
+                <title>Tunahan KAYA</title>
+                
+        </head>
+
+        <body>
+
+            <script type="text/javascript" language="javascript">
+                //yorum satýrý
+
+
+                /*
+                çok satýrlý yorum 
+                asdf
+                adasdasd
+                adsdadasdasf
+                */
+
+            </script>
+
+        </body>
+</html>
+
+
