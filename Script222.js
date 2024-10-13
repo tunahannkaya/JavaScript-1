4e187cf0ad2b4eb3edcb79b5275fdff6d9404c3a
tunahannkaya <152183813+tunahannkaya@users.noreply.github.com>
2024-10-13 12:20:09 +0300

    Studies
    Başlangıç seviyesi

---
+++ Script222.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+< !DOCTYPE html >
+    <html lang="tr-TR">
+        <head>
+            <meta http-equiv="content-type" content="text/html; charset=utf-8">
+                <title>Tunahan KAYA</title>
+        </head>
+
+        <body>
+
+            <noscript>
+
+                Sayin kulanicimiz, tarayicinizde JavaScript kodlari calistirmasi pasif durumdadýr
+
+            </noscript>
+
+        </body>
+    </html>
